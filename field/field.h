c28a40e1eb208dcf46a55effe8029767d209ede0
shevchenko-nikita <nicksevchenkos@gmail.com>
2024-12-10 00:13:53 +0200

    Shape structure reposition

--- field/field.h
+++ field/field.h
@@ -1,18 +1,9 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
+#include "../shape/shape.h"
 
 #include <vector>
 #include <cstdlib>
-
-const std::array<sf::Color, 5> COLORS =
-{
-    sf::Color(3, 65, 174),
-    sf::Color(114, 203, 59),
-    sf::Color(255, 213, 0),
-    sf::Color(255, 151, 28),
-    sf::Color(255, 50, 19)
-};
 
 enum class MoveDirection
 {
@@ -21,48 +12,6 @@ enum class MoveDirection
     LEFT,
     RIGHT
 };
-
-namespace Shape
-{
-    struct Coordinate
-    {
-        int x;
-        int y;
-    };
-
-    const std::vector<std::vector<Coordinate>> ALL_SHAPES =
-    {
-            {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
-            {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
-            {{1, 0}, {1, 1}, {1, 2}, {0, 2}},
-            {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
-            {{1, 0}, {1, 1}, {0, 1}, {0, 2}},
-            {{0, 1}, {1, 0}, {1, 1}, {1, 2}},
-            {{0, 0}, {0, 1}, {1, 1}, {1, 2}}
-    };
-
-    struct Shape
-    {
-        Shape()
-        {
-            Change();
-        }
-
-        void Change()
-        {
-            static int colorId = 0;
-            int shapeId = rand() % 7;
-
-            blocks = ALL_SHAPES[shapeId];
-            color = COLORS[colorId];
-            colorId = (colorId + 1) % 5;
-        }
-
-        std::vector<Coordinate> blocks;
-        sf::Color color;
-    };
-};
-
 
 class Field
 {
--- shape/shape.h
+++ shape/shape.h
@@ -1,67 +1,57 @@
-//#pragma once
-//
-//#include <vector>
-//#include <cstdlib>
-//
-//enum class BlockColors
-//{
-//    NONE,
-//    BLUE, // #0341AE
-//    GREEN, // #72CB3B
-//    YELLOW, // #FFD500
-//    ORANGE, // #FF971C
-//    RED // FF3213
-//};
-//
-//namespace Shape
-//{
-//    enum class MoveDirection
-//    {
-//        NONE,
-//        DOWN,
-//        LEFT,
-//        RIGHT
-//    };
-//
-//    struct Coordinate
-//    {
-//        int x;
-//        int y;
-//    };
-//
-//    const std::vector<std::vector<Coordinate>> ALL_SHAPES =
-//            {
-//                    {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
-//                    {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
-//                    {{1, 0}, {1, 1}, {1, 2}, {0, 2}},
-//                    {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
-//                    {{1, 0}, {1, 1}, {0, 1}, {0, 2}},
-//                    {{0, 1}, {1, 0}, {1, 1}, {1, 2}},
-//                    {{0, 0}, {0, 1}, {1, 1}, {1, 2}}
-//            };
-//
-//    class Shape
-//    {
-//    public:
-//        Shape()
-//        {
-//            Change();
-//        }
-//
-//        bool Change()
-//        {
-//            int shape_id = rand() % 7;
-//            blocks = ALL_SHAPES[shape_id];
-//            color = static_cast<BlockColors>(rand() % 6);
-//            return true;
-//        }
-//
-//        bool CanMove(Shape::Shape& shape, MoveDirection direction);
-//        bool Move(Shape::Shape& shape, MoveDirection direction);
-//
-//    private:
-//
-//        std::vector<Coordinate> blocks;
-//        BlockColors color;
-//    };
-//};
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <array>
+#include <vector>
+
+const std::array<sf::Color, 5> COLORS =
+{
+        sf::Color(3, 65, 174),
+        sf::Color(114, 203, 59),
+        sf::Color(255, 213, 0),
+        sf::Color(255, 151, 28),
+        sf::Color(255, 50, 19)
+};
+
+namespace Shape
+{
+    struct Coordinate
+    {
+        int x;
+        int y;
+    };
+
+    const std::vector<std::vector<Coordinate>> ALL_SHAPES =
+            {
+                    {{4, 0}, {5, 0}, {5, 1}, {5, 2}},
+                    {{4, 0}, {4, 1}, {4, 2}, {4, 3}},
+                    {{5, 0}, {5, 1}, {5, 2}, {4, 2}},
+                    {{4, 0}, {4, 1}, {5, 0}, {5, 1}},
+                    {{5, 0}, {5, 1}, {4, 1}, {4, 2}},
+                    {{4, 1}, {5, 0}, {5, 1}, {5, 2}},
+                    {{4, 0}, {4, 1}, {5, 1}, {5, 2}}
+            };
+
+    struct Shape
+    {
+        Shape()
+        {
+            Change();
+        }
+
+        void Change()
+        {
+            static int colorId = 0;
+            int shapeId = rand() % 7;
+
+            blocks = ALL_SHAPES[shapeId];
+            color = COLORS[colorId];
+            colorId = (colorId + 1) % 5;
+        }
+
+        std::vector<Coordinate> blocks;
+        sf::Color color;
+    };
+};
